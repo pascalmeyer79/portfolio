56175ce99a2363f0357b32045c0827e8fc2a4615
Pascal Meyer <hi@pascalmey.com>
2026-01-21 21:40:08 +0100

    Redirect-Regel aus next.config.mjs entfernt, da Middleware das übernimmt

--- next.config.mjs
+++ next.config.mjs
@@ -4,21 +4,6 @@ const nextConfig = {
   images: {
     qualities: [100, 75],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'pascalmey.com',
-          },
-        ],
-        destination: 'https://www.pascalmey.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
